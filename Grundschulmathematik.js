386c14b232b3db3a48d6a73ecf88978c8c81b45f
unknown <julia.wienkop@gmail.com>
2020-10-07 13:28:28 +0200

    added equation for addition and random number

--- Grundschulmathematik.js
+++ Grundschulmathematik.js
@@ -1,3 +1,32 @@
-function myFunction() {
+
     alert("Grundschulmathe");
-}  
+
+var min = 1;
+var max = 20;
+
+function rand(min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+function zufall() {
+    var number1 = rand(1, 20);
+    var number2 = rand(1, 20);
+    var result = number1 + number2;
+
+    var Ergebnis = prompt("Wie viel ist " + number1 + " + " + number2);
+
+    if (Ergebnis != null) {
+        document.getElementById("demo").innerHTML =
+            "Your result was " + Ergebnis;
+    }
+
+    if (result == Ergebnis) {
+        alert("true");
+    }
+    else {
+        alert("false");
+    }
+}
