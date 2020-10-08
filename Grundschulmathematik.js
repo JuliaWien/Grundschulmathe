bcd4c1740a26ecd8d681cbfa8b2773e739ee7a6b
unknown <julia.wienkop@gmail.com>
2020-10-08 18:48:27 +0200

    fullis task c

--- Grundschulmathematik.js
+++ Grundschulmathematik.js
@@ -1,14 +1,16 @@
 
-    alert("Grundschulmathe");
+alert("Grundschulmathe");
 
 var min = 1;
 var max = 20;
 
-function rand(min, max)
-{
+function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+var correctCount = 0;
+var testsCount = 0;
 
 
 function zufall() {
@@ -16,17 +18,41 @@ function zufall() {
     var number2 = rand(1, 20);
     var result = number1 + number2;
 
+    testsCount++
     var Ergebnis = prompt("Wie viel ist " + number1 + " + " + number2);
 
-    if (Ergebnis != null) {
-        document.getElementById("demo").innerHTML =
-            "Your result was " + Ergebnis;
-    }
-
     if (result == Ergebnis) {
-        alert("true");
-    }
-    else {
-        alert("false");
+        correctCount++;
+        alert("true " + correctCount + "/5");
+        if (testsCount == 5) {
+            if (correctCount >= 4) {
+                correctCount = 0;
+                alert("Done");
+            }
+            else {
+                alert("go and learn again");
+                correctCount = 0;
+            }
+        }
+        else {
+            if (testsCount == 5) {
+                if (correctCount >= 4) {
+                    correctCount = 0;
+                    alert("Done");
+                }
+                else {
+                    alert("go and learn again");
+                    correctCount = 0;
+                }
+            }
+        }
+
+        if (Ergebnis != null) {
+            document.getElementById("demo").innerHTML =
+                "Your result was " + Ergebnis + " You got " + correctCount + "/5";
+        }
+        if (testsCount == 5) {
+            testsCount = 0;
+        }
     }
 }
