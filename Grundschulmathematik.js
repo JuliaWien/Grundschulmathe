5e4afa56677a61e98c07cbee452e3d67d12bc995
unknown <julia.wienkop@gmail.com>
2020-10-10 19:26:42 +0200

    fullfills task d

--- Grundschulmathematik.js
+++ Grundschulmathematik.js
@@ -1,58 +1,144 @@
-
 alert("Grundschulmathe");
 
-var min = 1;
-var max = 20;
-
+//benötigte Variablen
+var correctCount = 0;
+var testsCount = 0;
+var levelCount = 0;
+var rechenzeichen;
+//Ermitteln der Zufallszahl
 function rand(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function rechenaufgabe() 
+{
+    switch (levelCount) {
+        case 0:
+            rechenzeichen = "+";
 
-var correctCount = 0;
-var testsCount = 0;
+            var number1 = rand(1, 20);
+            var number2 = rand(1, 20);
+            var result = number1 + number2;
+
+            var feed = prompt("Wie viel ist " + number1 + rechenzeichen + number2);
+            break;
+
+        case 1:
+            rechenzeichen ="-"
+
+            var number1 = rand(1, 20);
+            var number2 = rand(1, number1);
+
+            var result = number1 - number2;
+
+            
+
+            var feed = prompt("Wie viel ist " + number1 + rechenzeichen + number2);
+            break;
+    }
 
 
-function zufall() {
-    var number1 = rand(1, 20);
-    var number2 = rand(1, 20);
-    var result = number1 + number2;
-
-    testsCount++
-    var Ergebnis = prompt("Wie viel ist " + number1 + " + " + number2);
-
-    if (result == Ergebnis) {
+    if (result == feed) {
         correctCount++;
         alert("true " + correctCount + "/5");
-        if (testsCount == 5) {
-            if (correctCount >= 4) {
-                correctCount = 0;
-                alert("Done");
-            }
-            else {
-                alert("go and learn again");
-                correctCount = 0;
-            }
-        }
-        else {
-            if (testsCount == 5) {
-                if (correctCount >= 4) {
-                    correctCount = 0;
-                    alert("Done");
-                }
-                else {
-                    alert("go and learn again");
-                    correctCount = 0;
-                }
-            }
+    }
+    else {
+        alert("false " + correctCount + "/5");
+    }
+
+    if (feed != null)
+        {
+            document.getElementById("demo").innerHTML =
+                "Your result was " + feed + " You got " + correctCount + "/5";
         }
 
-        if (Ergebnis != null) {
-            document.getElementById("demo").innerHTML =
-                "Your result was " + Ergebnis + " You got " + correctCount + "/5";
+    testsCount++;
+    if (testsCount == 5)
+     {
+        if (correctCount >= 4) 
+        {
+            alert("Well, done you have reached the next level");
+            levelCount++;
+            correctCount = 0;
+            testsCount = 0;
+            alert (levelCount);
         }
-        if (testsCount == 5) {
+        else {
+            alert("To reach the next level you need to try it again");
+            correctCount = 0;
             testsCount = 0;
         }
     }
+    if (levelCount == 2)
+    {
+        levelCount == 0;
+    }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*do { // Dies ist die äußere Schleife
+    for (testsCount = 0; testsCount < 5; testsCount++) {
+        alert(testsCount);
+        if (result == feed)
+        {
+            correctCount++;
+            alert("true " + correctCount + "/5");
+            alert(testsCount);
+        }
+        else {
+            alert("false");
+        }
+        if (feed != null)
+        {
+            document.getElementById("demo").innerHTML =
+                "Your result was " + input + " You got " + correctCount + "/5";
+        }
+    }
+    if (testsCount == 5) {
+        if (correctCount >= 4)
+        {
+            alert("Well, done you have reached the next level");
+            levelCount++;
+            correctCount = 0;
+
+        }
+        else
+        {
+            alert("To reach the next level you need to try it again");
+            correctCount = 0;
+        }
+    }
+
+} while (true);
 }
+*/
